50975e69e8dfd3b99af916ed0cfb3d2521d9f24c
siminniu <146621079+siminniu@users.noreply.github.com>
2024-02-04 03:01:26 +0800

    Add files via upload

---
+++ PG_GEN/TopicFusion.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+import networkx as nx
+from py2neo import Graph
+from PGRAG.PG_GEN.embedding import TextSimilarityCalculator
+
+
+class TopicFusionManager:
+    def __init__(self):
+        """
+        初始化，连接到Neo4j数据库。
+        """
+        self.graph = Graph("bolt://localhost:7687", auth=("neo4j", "1234"))
+        self.sim_calculator = TextSimilarityCalculator()
+
+    def keyword_fusion(self):
+        """
+        执行关键词融合过程。
+        创建主题之间的连接基于共享的关键词，并创建超主题节点。
+        """
+        # 提取所有 Topic 节点的关键词
+        query = "MATCH (n:Topic) RETURN n.主题, n.关键词"
+        topics = self.graph.run(query).data()
+
+        # 创建主题节点到关键词的映射
+        topic_keywords = {topic['n.主题']: set(topic['n.关键词'].split()) for topic in topics if topic['n.关键词']}
+
+        # 使用 NetworkX 创建图
+        G = nx.Graph()
+
+        # 在共享关键词的主题节点之间添加边
+        for topic1, keywords1 in topic_keywords.items():
+            G.add_node(topic1, keywords=keywords1)
+            for topic2, keywords2 in topic_keywords.items():
+                if topic1 != topic2 and len(keywords1 & keywords2) >= 3:  # 修改条件，至少两个关键词的重叠
+                    G.add_edge(topic1, topic2)
+
+        # 为每个连通分量创建一个超主题节点并连接相关的主题节点
+        connected_components = list(nx.connected_components(G))
+        for component in connected_components:
+            super_topic_keywords = set()
+            for topic in component:
+                super_topic_keywords.update(G.nodes[topic]['keywords'])
+            super_topic_keywords = super_topic_keywords.pop()
+            # super_topic_keywords = ' '.join(super_topic_keywords)
+            # 创建超主题节点
+            super_topic_query = f"MERGE (s:SuperTopic {{关键词: '{super_topic_keywords}'}}) RETURN s"
+            super_topic = self.graph.run(super_topic_query).evaluate()
+            print(super_topic,"插入成功！")
+            # 将 Topic 节点连接到 SuperTopic
+            for topic_title in component:
+                relation_query = f"""
+                MATCH (t:Topic {{主题: $topic_title}}), (s:SuperTopic {{关键词: $super_topic_keywords}})
+                MERGE (t)-[:属于]->(s)
+                """
+                self.graph.run(relation_query, topic_title=topic_title, super_topic_keywords=super_topic_keywords)
+
+        print("关键词融合完成")
+
+    # def keyword_fusion(self):
+    #     """
+    #     执行关键词融合过程。
+    #     创建主题之间的连接基于共享的关键词，并创建超主题节点。
+    #     """
+    #     # 提取所有 Topic 节点的关键词
+    #     query = "MATCH (n:Topic) RETURN n.主题, n.关键词"
+    #     topics = self.graph.run(query).data()
+    #
+    #     # 创建主题节点到关键词的映射
+    #     topic_keywords = {topic['n.主题']: set(topic['n.关键词'].split()) for topic in topics if topic['n.关键词']}
+    #
+    #     # 使用 NetworkX 创建图
+    #     G = nx.Graph()
+    #
+    #     # 在共享关键词的主题节点之间添加边
+    #     for topic1, keywords1 in topic_keywords.items():
+    #         G.add_node(topic1, keywords=keywords1)
+    #         for topic2, keywords2 in topic_keywords.items():
+    #             if topic1 != topic2 and keywords1 & keywords2:
+    #                 G.add_edge(topic1, topic2)
+    #
+    #     # 为每个连通分量创建一个超主题节点并连接相关的主题节点
+    #     connected_components = list(nx.connected_components(G))
+    #     for component in connected_components:
+    #         super_topic_keywords = set()
+    #         for topic in component:
+    #             super_topic_keywords.update(G.nodes[topic]['keywords'])
+    #         super_topic_keywords = super_topic_keywords.pop()
+    #         # super_topic_keywords = ' '.join(super_topic_keywords)
+    #         # 创建超主题节点
+    #         super_topic_query = f"MERGE (s:SuperTopic {{关键词: '{super_topic_keywords}'}}) RETURN s"
+    #         super_topic = self.graph.run(super_topic_query).evaluate()
+    #         print(super_topic,"插入成功！")
+    #         # 将 Topic 节点连接到 SuperTopic
+    #         for topic_title in component:
+    #             relation_query = f"""
+    #             MATCH (t:Topic {{主题: $topic_title}}), (s:SuperTopic {{关键词: $super_topic_keywords}})
+    #             MERGE (t)-[:属于]->(s)
+    #             """
+    #             self.graph.run(relation_query, topic_title=topic_title, super_topic_keywords=super_topic_keywords)
+    #
+    #     print("关键词融合完成")
+
+    def similarity_fusion(self):
+        """
+        执行相似性融合过程。
+        对每个单一主题节点，检查其与其他超主题节点下的主题节点的相似性，并进行适当的更新。
+        """
+        # 第一步：获取所有单一主题的超主题节点
+        query_single = """
+        MATCH (t:Topic)-[:属于]->(st:SuperTopic)
+        WITH st, count(t) AS topics_count
+        WHERE topics_count = 1
+        RETURN st.关键词 AS super_topic_keywords
+        """
+        single_super_topics = self.graph.run(query_single).data()
+
+        # 第二步：对每个单一主题节点，检查其与其他超主题节点下的主题节点的相似性
+        for single_super_topic in single_super_topics:
+            # 获取单一主题节点的嵌入和名称
+            query_t = """
+            MATCH (t:Topic)-[:属于]->(st:SuperTopic {关键词: $keywords})
+            RETURN t.主题 AS topic
+            """
+            single_topic_name = self.graph.run(query_t, keywords=single_super_topic['super_topic_keywords']).evaluate()
+
+            query_te = """
+            MATCH (t:Topic)-[:属于]->(st:SuperTopic {关键词: $keywords})
+            RETURN t.主题嵌入 AS embedding
+            """
+            single_topic_embedding = self.graph.run(query_te,
+                                                    keywords=single_super_topic['super_topic_keywords']).evaluate()
+
+            # 获取其他所有超主题节点下的主题节点
+            query_others = """
+            MATCH (t:Topic)-[:属于]->(st:SuperTopic)
+            WHERE NOT st.关键词 = $keywords
+            RETURN t.主题嵌入 AS embedding, t.主题 AS topic
+            """
+            other_topics = self.graph.run(query_others, keywords=single_super_topic['super_topic_keywords']).data()
+
+            # 检查相似性
+            for other_topic in other_topics:
+                # print(type(single_topic_embedding),type(other_topic['embedding']))
+                try:
+                    similarity = self.sim_calculator.calculate_similarity_from_embedding(eval(single_topic_embedding),
+                                                                                     eval(other_topic['embedding']))
+                except:
+                    print(single_topic_name,single_topic_embedding)
+                if similarity > 0.85:
+                    # 获取相似主题节点的超主题节点的关键词
+                    query_other_super_topic_keywords = """
+                    MATCH (t:Topic {主题: $topic})-[:属于]->(st:SuperTopic)
+                    RETURN st.关键词 AS super_topic_keywords
+                    """
+                    other_super_topic_keywords = self.graph.run(query_other_super_topic_keywords,
+                                                                topic=other_topic['topic']).evaluate()
+
+                    # 如果相似，更新超主题节点
+                    update_query = """
+                    MATCH (t:Topic {主题: $single_topic}), (st:SuperTopic {关键词: $other_super_keywords})
+                    MERGE (t)-[:属于]->(st)
+                    RETURN st
+                    """
+                    self.graph.run(update_query, single_topic=single_topic_name,
+                                   other_super_keywords=other_super_topic_keywords)
+                    print('插入成功！')
+
+                    # 将single_topic的关键词添加到other_super_keywords的SuperTopic现有的关键词集合中
+                    merge_keywords_query = """
+                    MATCH (st1:SuperTopic {关键词: $single_keywords}), (st2:SuperTopic {关键词: $other_super_keywords})
+                    WITH st1, st2
+                    SET st2.关键词 = st2.关键词 + ' ' + st1.关键词
+                    DETACH DELETE st1
+                    """
+                    self.graph.run(merge_keywords_query, single_keywords=single_super_topic['super_topic_keywords'],
+                                   other_super_keywords=other_super_topic_keywords)
+
+                    break  # 找到相似节点后停止查找
+
+        print("相似性融合完成")
---
+++ PG_GEN/neo4j_data_prepare.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+from py2neo import Graph
+from PGRAG.PG_GEN.embedding import TextSimilarityCalculator
+from concurrent.futures import ThreadPoolExecutor
+
+class JsonToKG:
+    def __init__(self):
+        """
+        初始化连接到Neo4j数据库和文本相似度计算器。
+        """
+        self.graph = Graph("bolt://localhost:7687", auth=("neo4j", "1234"))
+        self.sim_calculator = TextSimilarityCalculator()
+
+    def recursive_json_iterator(self, json_data, path='', topic_paths=None):
+        """
+        递归解析JSON数据。
+        输入: json_data - JSON格式的数据
+        输出: topic_paths - 包含所有路径的列表
+        """
+        if topic_paths is None:
+            topic_paths = []
+
+        if isinstance(json_data, dict):
+            for key, value in json_data.items():
+                current_path = f"{path} '{key}'".lstrip()
+                self.recursive_json_iterator(value, current_path, topic_paths)
+        elif isinstance(json_data, list):
+            topic_path = f"{path} '{' '.join(map(str, json_data))}'".lstrip()
+            topic_paths.append(topic_path)
+        else:
+            topic_path = f"{path} '{json_data}'".lstrip()
+            topic_paths.append(topic_path)
+
+        return topic_paths
+
+    def load_json_files(self, raw_doc_directory_path):
+        """
+        从指定目录加载JSON文件。
+        输入: raw_doc_directory_path - JSON文件所在的目录路径
+        输出: all_json_contents - 包含所有JSON内容的字典
+        """
+        json_files = [file for file in os.listdir(raw_doc_directory_path) if file.endswith('.json')]
+        all_json_contents = {}
+
+        for json_file in json_files:
+            file_path = os.path.join(raw_doc_directory_path, json_file)
+            with open(file_path, 'r', encoding='utf-8') as file:
+                content = file.read()
+                try:
+                    # 尝试解析JSON数据
+                    parsed_line = json.loads(content)
+                except json.JSONDecodeError as e:
+                    print(f'JSON解析错误在文件{json_file}', e)
+                # parsed_line = ast.literal_eval(file.readline())
+                all_json_contents[json_file] = parsed_line
+
+        return all_json_contents
+
+    def process_and_insert_data(self, raw_doc_directory_path):
+        """
+        处理并插入数据到Neo4j数据库。
+        输入: raw_doc_directory_path - JSON文件所在的目录路径
+        输出: 无（数据被插入到数据库）
+        """
+        result = self.load_json_files(raw_doc_directory_path)
+
+        for i in list(result.keys()):
+            json_data = result.get(i, {})
+            emb_t = self.sim_calculator.calculate_embedding(str(json_data))
+
+            document_properties = {
+                "主题": json_data.get("主题", None),
+                "主题嵌入": str(emb_t),
+                "关键词": json_data.get("元信息", {}).get("关键词", None)
+            }
+
+            label = "Topic"
+            primary_key = "主题"
+            topic_paths = self.recursive_json_iterator(json_data.get("详情", {}))
+
+            for key, value in document_properties.items():
+                if value:
+                    update_query = f"MERGE (d:{label} {{{primary_key}: $primary_key_value}}) SET d.{key} = $value RETURN d"
+                    updated_node = self.graph.run(update_query, parameters={"primary_key_value": document_properties[primary_key], "value": value}).evaluate()
+
+            topic_name = updated_node.get('主题')
+            print(f'名为“{topic_name}”的主题节点插入成功！')
+            # ...
+            for topic_path in topic_paths:
+                split_parts = topic_path.strip().strip("'").split("' '")
+                parts = [part.strip() for part in split_parts if part.strip()]
+                print('-------------------------------')
+                print('待插入主题路径：', parts)
+
+                # 插入子主题
+                for j, sub_topic_type in enumerate(parts[:-1]):
+                    if j == 0:
+                        # 新的第一个子主题
+                        create_TST_query = "MATCH (d:Topic {主题: $topic_name}) MERGE (d)-[r:基础链接]->(st:SubTopic {路标: $sub_topic_type}) RETURN d, st"
+                        TST_result = self.graph.run(create_TST_query,
+                                                    parameters={"topic_name": topic_name, "sub_topic_type": sub_topic_type}).data()
+                        print('主题到子主题插入成功！结果显示：', TST_result)
+                    else:
+                        # 新的中间子主题
+                        match_query_parts = [f"-[r{k}:基础链接]->(pst{k}:SubTopic {{路标: $part{k}}})" for k, part in
+                                             enumerate(parts[:j], 1)]
+                        match_query = "MATCH (d:Topic {主题: $topic_name}) " + ''.join(match_query_parts)
+                        merge_query = f" WITH pst{j} MERGE (pst{j})-[r:基础链接]->(st:SubTopic {{路标: $sub_topic_type}}) RETURN st"
+                        create_STST_query = match_query + merge_query
+
+                        params = {"topic_name": topic_name, "sub_topic_type": sub_topic_type}
+                        for k, part in enumerate(parts[:j], 1):
+                            params[f"part{k}"] = part
+
+                        STST_result = self.graph.run(create_STST_query, parameters=params).data()
+                        print('插入的主题路径：', STST_result)
+                    if j == len(parts) - 2:
+                        # 叶子主题
+                        emb_fp = self.sim_calculator.calculate_embedding(parts[0] + ' '.join(parts[1:]))
+                        fact = parts[j + 1]
+                        match_query_parts = [f"-[r{k}:基础链接]->(pst{k}:SubTopic {{路标: $part{k}}})" for k, part in
+                                             enumerate(parts[:j + 1], 1)]
+                        match_query = f"MATCH (d:Topic {{主题: $topic_name}}) " + ''.join(match_query_parts)
+                        merge_query = f" MERGE (c:Content {{事实: $fact, 路径嵌入: $fp}}) WITH pst{j + 1}, c MERGE (pst{j + 1})-[r:基础链接]->(c) RETURN c"
+
+                        create_STC_query = match_query + merge_query
+
+                        params = {"topic_name": topic_name, "fact": fact, "fp": str(emb_fp)}
+
+                        for k, part in enumerate(parts[:j + 1], 1):
+                            params[f"part{k}"] = part
+
+                        STC_result = self.graph.run(create_STC_query, parameters=params).data()
+                        print("**完整的路径插入成功！结果显示：", STC_result)
+
+    def update_single_subtopic_embedding(self, subtopic_path):
+        """
+        更新单个子主题节点的路由嵌入。
+        """
+        subtopic = subtopic_path['st']
+        path_names = subtopic_path['path_names']
+
+        path_str = ' '.join(path_names)
+        embedding = self.sim_calculator.calculate_embedding(path_str)
+        print(path_str)
+        query_update_embedding = """
+        MATCH path = (t:Topic)-[:基础链接*]->(st:SubTopic {路标: $subtopic_label})
+        WHERE [node IN nodes(path) | CASE WHEN node:Topic THEN node.主题 WHEN node:SubTopic THEN node.路标 END] = $path_names
+        SET st.路由嵌入 = $embedding 
+        RETURN count(st) as updated
+        """
+        result = self.graph.run(query_update_embedding, subtopic_label=subtopic['路标'], path_names=path_names, embedding=str(embedding)).evaluate()
+
+        if result == 0:
+            print(f"Failed to update routing embedding for path: {path_str}")
+    def update_subtopic_embeddings(self):
+        query_subtopics_paths = """
+        MATCH path = (t:Topic)-[:基础链接*]->(st:SubTopic)
+        WHERE not EXISTS(st.路由嵌入) 
+        RETURN st, [node IN nodes(path) | CASE WHEN node:Topic THEN node.主题 WHEN node:SubTopic THEN node.路标 END] AS path_names
+        """
+        subtopics_paths = self.graph.run(query_subtopics_paths).data()
+        # 使用 ThreadPoolExecutor 来限制线程数量
+        with ThreadPoolExecutor(max_workers=14) as executor:  # 你可以根据需要调整 max_workers 的值
+            executor.map(self.update_single_subtopic_embedding, subtopics_paths)
+
+
+
+
+    # def update_subtopic_embeddings(self):
+    #     """
+    #     更新所有子主题节点的路由嵌入。
+    #     输入: 无
+    #     输出: 无（更新数据库中的节点）
+    #     """
+    #     query_subtopics_paths = """
+    #     MATCH path = (t:Topic)-[:基础链接*]->(st:SubTopic)
+    #     RETURN st, [node IN nodes(path) | CASE WHEN node:Topic THEN node.主题 WHEN node:SubTopic THEN node.路标 END] AS path_names
+    #     """
+    #     subtopics_paths = self.graph.run(query_subtopics_paths).data()
+    #
+    #     for subtopic_path in subtopics_paths:
+    #         subtopic = subtopic_path['st']
+    #         path_names = subtopic_path['path_names']
+    #
+    #         path_str = ' '.join(path_names)
+    #
+    #         # 计算路径嵌入
+    #         embedding = self.sim_calculator.calculate_embedding(path_str)
+    #
+    #         # 更新特定子主题节点的“路由嵌入”属性
+    #         query_update_embedding = """
+    #         MATCH path = (t:Topic)-[:基础链接*]->(st:SubTopic {路标: $subtopic_label})
+    #         WHERE [node IN nodes(path) | CASE WHEN node:Topic THEN node.主题 WHEN node:SubTopic THEN node.路标 END] = $path_names
+    #         SET st.路由嵌入 = $embedding
+    #         RETURN count(st) as updated
+    #         """
+    #         result = self.graph.run(query_update_embedding, subtopic_label=subtopic['路标'], path_names=path_names, embedding=str(embedding)).evaluate()
+    #
+    #         if result == 0:
+    #             print(f"Failed to update routing embedding for path: {path_str}")
+
+
+# 示例使用
+# dm = JsonToKG()
+# raw_doc_directory_path = r'../data_old/bm/mindmap_old_1'
+# dm.process_and_insert_data(raw_doc_directory_path)
+# dm.update_subtopic_embeddings()
